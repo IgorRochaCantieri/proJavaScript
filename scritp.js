176dc0f6172e3bcfafaf27106b2bb94d52814b7f
Igor Rocha Cantieri <igorrocha2198@gmail.com>
2025-06-28 13:57:26 -0300

    modificação

---
+++ scritp.js
@@ -0,0 +1,11 @@
+function abrirForm(){
+    window.open("formulário/index.html", "_blanck")
+}
+
+function abrirLista(){
+    window.location.href = "lista/index.html"
+}
+
+function abrirJogoSecreto(){
+    window.open("jogoNumSecreto/index.html")
+}
